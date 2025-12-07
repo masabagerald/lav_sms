0670f6ebb68ea4c3d76f8531d81973102ab84db7
bwambalecrispus <bwambalecrispus@gmail.com>
2025-12-07 10:48:47 +0300

    changes in settings

--- database/seeders/SettingsTableSeeder.php
+++ database/seeders/SettingsTableSeeder.php
@@ -16,25 +16,21 @@ class SettingsTableSeeder extends Seeder
         DB::table('settings')->delete();
 
         $data = [
-            ['type' => 'current_session', 'description' => '2018-2019'],
-            ['type' => 'system_title', 'description' => 'CJIA'],
-            ['type' => 'system_name', 'description' => 'CJ INSPIRED ACADEMY'],
-            ['type' => 'term_ends', 'description' => '7/10/2018'],
-            ['type' => 'term_begins', 'description' => '7/10/2018'],
+            ['type' => 'current_session', 'description' => '2026'],
+            ['type' => 'system_title', 'description' => 'MPSS'],
+            ['type' => 'system_name', 'description' => 'MUBENDE PARENTS SECONDARY SCHOOL'],
+            ['type' => 'term_ends', 'description' => '1/1/2026'],
+            ['type' => 'term_begins', 'description' => '1/5/2026'],
             ['type' => 'phone', 'description' => '0123456789'],
-            ['type' => 'address', 'description' => '18B North Central Park, Behind Central Square Tourist Center'],
-            ['type' => 'system_email', 'description' => 'cjacademy@cj.com'],
+            ['type' => 'address', 'description' => 'MUBENDE MUNICIPALITY'],
+            ['type' => 'system_email', 'description' => 'mpsschool@mpss.com'],
             ['type' => 'alt_email', 'description' => ''],
             ['type' => 'email_host', 'description' => ''],
             ['type' => 'email_pass', 'description' => ''],
             ['type' => 'lock_exam', 'description' => 0],
             ['type' => 'logo', 'description' => ''],
-            ['type' => 'next_term_fees_j', 'description' => '20000'],
-            ['type' => 'next_term_fees_pn', 'description' => '25000'],
-            ['type' => 'next_term_fees_p', 'description' => '25000'],
-            ['type' => 'next_term_fees_n', 'description' => '25600'],
-            ['type' => 'next_term_fees_s', 'description' => '15600'],
-            ['type' => 'next_term_fees_c', 'description' => '1600'],
+            ['type' => 'next_term_fees_o', 'description' => '20000'],
+            ['type' => 'next_term_fees_a', 'description' => '25000'],
         ];
 
         DB::table('settings')->insert($data);
