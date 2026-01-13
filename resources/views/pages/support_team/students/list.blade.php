b73e246f79ef6b0e72d15ba5a13571095ea5f9c5
Gerald Masaba <masabagerald@gmail.com>
2026-01-13 10:52:35 +0300

    making the progress centered

--- resources/views/pages/support_team/students/list.blade.php
+++ resources/views/pages/support_team/students/list.blade.php
@@ -4,21 +4,50 @@
 
 @section('content')
 
+{{-- =========================
+    STYLES
+========================== --}}
+<style>
+#importOverlay {
+    position: fixed;
+    inset: 0;
+    z-index: 9999;
+}
+.import-overlay-backdrop {
+    position: absolute;
+    inset: 0;
+    background: rgba(0,0,0,0.45);
+}
+.import-overlay-card {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background: #fff;
+    padding: 25px 30px;
+    border-radius: 8px;
+    width: 360px;
+    text-align: center;
+    box-shadow: 0 10px 40px rgba(0,0,0,0.3);
+}
+</style>
+
 <div class="card">
     <div class="card-header header-elements-inline">
         <h6 class="card-title">Students List</h6>
-        <div class="header-elements">
-        @if(Qs::userIsTeamSA())
-            <button type="button"
-                    class="btn btn-sm btn-primary mr-2"
-                    data-toggle="modal"
-                    data-target="#bulkUploadModal">
-                <i class="icon-upload mr-1"></i> Bulk Upload
-            </button>
-        @endif
 
-        {!! Qs::getPanelOptions() !!}
-    </div>
+        <div class="header-elements">
+            @if(Qs::userIsTeamSA())
+                <button type="button"
+                        class="btn btn-sm btn-primary mr-2 lock-during-import"
+                        data-toggle="modal"
+                        data-target="#bulkUploadModal">
+                    <i class="icon-upload mr-1"></i> Bulk Upload
+                </button>
+            @endif
+
+            {!! Qs::getPanelOptions() !!}
+        </div>
     </div>
 
     <div class="card-body">
@@ -73,12 +102,9 @@
                             <td>{{ $loop->iteration }}</td>
 
                             <td>
-                                <img
-                                    class="rounded-circle"
-                                    style="height:40px;width:40px;object-fit:cover;"
-                                    src="{{ optional($s->user)->photo ?? asset('images/default-user.png') }}"
-                                    alt="photo"
-                                >
+                                <img class="rounded-circle"
+                                     style="height:40px;width:40px;object-fit:cover;"
+                                     src="{{ optional($s->user)->photo ?? asset('images/default-user.png') }}">
                             </td>
 
                             <td>{{ $s->user->name ?? '' }}</td>
@@ -87,57 +113,15 @@
                             <td>{{ $s->user->gender ?? '-' }}</td>
                             <td>{{ $s->age ?? '-' }}</td>
                             <td>{{ $s->house ?? '-' }}</td>
-
                             <td>{{ $s->fees ?? '-' }}</td>
-
                             <td>{{ $s->year_admitted ?? '-' }}</td>
 
                             <td class="text-center">
                                 <div class="list-icons">
-                                    <div class="dropdown">
-                                        <a href="#" class="list-icons-item" data-toggle="dropdown">
-                                            <i class="icon-menu9"></i>
-                                        </a>
-
-                                        <div class="dropdown-menu dropdown-menu-left">
-                                            <a href="{{ route('students.show', Qs::hash($s->id)) }}" class="dropdown-item">
-                                                <i class="icon-eye"></i> View Profile
-                                            </a>
-
-                                            @if(Qs::userIsTeamSA())
-                                                <a href="{{ route('students.edit', Qs::hash($s->id)) }}" class="dropdown-item">
-                                                    <i class="icon-pencil"></i> Edit
-                                                </a>
-
-                                                <a href="{{ route('st.reset_pass', Qs::hash($s->user->id)) }}" class="dropdown-item">
-                                                    <i class="icon-lock"></i> Reset Password
-                                                </a>
-                                            @endif
-
-                                            <a target="_blank"
-                                               href="{{ route('marks.year_selector', Qs::hash($s->user->id)) }}"
-                                               class="dropdown-item">
-                                                <i class="icon-check"></i> Marksheet
-                                            </a>
-
-                                            @if(Qs::userIsSuperAdmin())
-                                                <a id="{{ Qs::hash($s->user->id) }}"
-                                                   onclick="confirmDelete(this.id)"
-                                                   href="#"
-                                                   class="dropdown-item text-danger">
-                                                    <i class="icon-trash"></i> Delete
-                                                </a>
-
-                                                <form method="post"
-                                                      id="item-delete-{{ Qs::hash($s->user->id) }}"
-                                                      action="{{ route('students.destroy', Qs::hash($s->user->id)) }}"
-                                                      class="d-none">
-                                                    @csrf
-                                                    @method('delete')
-                                                </form>
-                                            @endif
-                                        </div>
-                                    </div>
+                                    <a href="#" class="list-icons-item lock-during-import"
+                                       data-toggle="dropdown">
+                                        <i class="icon-menu9"></i>
+                                    </a>
                                 </div>
                             </td>
                         </tr>
@@ -146,100 +130,6 @@
                 </table>
             </div>
 
-            {{-- ================= SECTION TABS ================= --}}
-            @foreach($sections as $se)
-                <div class="tab-pane fade" id="s{{ $se->id }}">
-                    <table class="table datatable-button-html5-columns">
-                        <thead>
-                        <tr>
-                            <th>S/N</th>
-                            <th>Photo</th>
-                            <th>Name</th>
-                            <th>ADM No</th>
-                            <th>Gender</th>
-                            <th>Fees</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-
-                        <tbody>
-                        @foreach($students->where('section_id', $se->id) as $sr)
-                            <tr>
-                                <td>{{ $loop->iteration }}</td>
-
-                                <td>
-                                    <img
-                                        class="rounded-circle"
-                                        style="height:40px;width:40px;object-fit:cover;"
-                                        src="{{ optional($sr->user)->photo ?? asset('images/default-user.png') }}"
-                                    >
-                                </td>
-
-                                <td>{{ $sr->user->name ?? '' }}</td>
-                                <td>{{ $sr->adm_no }}</td>
-                                <td>{{ $sr->user->gender ?? '-' }}</td>
-
-                                <td>
-                                    {!! $sr->fees
-                                        ? '<span class="badge badge-success">Paid</span>'
-                                        : '<span class="badge badge-danger">Not Paid</span>' !!}
-                                </td>
-
-                                <td class="text-center">
-                                    <div class="list-icons">
-                                        <div class="dropdown">
-                                            <a href="#" class="list-icons-item" data-toggle="dropdown">
-                                                <i class="icon-menu9"></i>
-                                            </a>
-
-                                            <div class="dropdown-menu dropdown-menu-right">
-                                                <a href="{{ route('students.show', Qs::hash($sr->id)) }}" class="dropdown-item">
-                                                    <i class="icon-eye"></i> View Info
-                                                </a>
-
-                                                @if(Qs::userIsTeamSA())
-                                                    <a href="{{ route('students.edit', Qs::hash($sr->id)) }}" class="dropdown-item">
-                                                        <i class="icon-pencil"></i> Edit
-                                                    </a>
-
-                                                    <a href="{{ route('st.reset_pass', Qs::hash($sr->user->id)) }}" class="dropdown-item">
-                                                        <i class="icon-lock"></i> Reset Password
-                                                    </a>
-                                                @endif
-
-                                                <a target="_blank"
-                                                   href="{{ route('marks.year_selector', Qs::hash($sr->user->id)) }}"
-                                                   class="dropdown-item">
-                                                    <i class="icon-check"></i> Marksheet
-                                                </a>
-
-                                                @if(Qs::userIsSuperAdmin())
-                                                    <a id="{{ Qs::hash($sr->user->id) }}"
-                                                       onclick="confirmDelete(this.id)"
-                                                       href="#"
-                                                       class="dropdown-item text-danger">
-                                                        <i class="icon-trash"></i> Delete
-                                                    </a>
-
-                                                    <form method="post"
-                                                          id="item-delete-{{ Qs::hash($sr->user->id) }}"
-                                                          action="{{ route('students.destroy', Qs::hash($sr->user->id)) }}"
-                                                          class="d-none">
-                                                        @csrf
-                                                        @method('delete')
-                                                    </form>
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            @endforeach
-
         </div>
     </div>
 </div>
@@ -247,8 +137,8 @@
 {{-- =========================
     BULK UPLOAD MODAL
 ========================== --}}
-<div class="modal fade" id="bulkUploadModal" tabindex="-1" role="dialog">
-    <div class="modal-dialog modal-sm" role="document">
+<div class="modal fade" id="bulkUploadModal" tabindex="-1">
+    <div class="modal-dialog modal-sm">
         <div class="modal-content">
 
             <form method="POST"
@@ -256,122 +146,142 @@
                   enctype="multipart/form-data">
                 @csrf
 
-                {{-- Pass current class automatically --}}
                 <input type="hidden" name="my_class_id" value="{{ $my_class->id }}">
 
                 <div class="modal-header">
-                    <h5 class="modal-title">
-                        Bulk Upload – {{ $my_class->name }}
-                    </h5>
-                    <button type="button" class="close" data-dismiss="modal">
-                        <span>&times;</span>
-                    </button>
+                    <h5 class="modal-title">Bulk Upload – {{ $my_class->name }}</h5>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
 
                 <div class="modal-body">
-                    <div class="form-group">
-                        <label>Select Excel File</label>
-                        <input type="file"
-                               name="file"
-                               class="form-control"
-                               accept=".xls,.xlsx"
-                               required>
-
-                        <small class="text-muted">
-                            EXCEL only
-                        </small>
-                    </div>
+                    <input type="file"
+                           name="file"
+                           class="form-control"
+                           accept=".xls,.xlsx"
+                           required>
+                    <small class="text-muted">Excel files only</small>
                 </div>
 
                 <div class="modal-footer">
-                   <button type="submit" class="btn btn-primary" id="uploadBtn">
+                    <button type="submit"
+                            class="btn btn-primary"
+                            id="uploadBtn">
                         Upload Students
                     </button>
-
-                    <button type="button" class="btn btn-light" data-dismiss="modal">
+                    <button type="button"
+                            class="btn btn-light"
+                            data-dismiss="modal">
                         Cancel
                     </button>
                 </div>
 
             </form>
-
         </div>
     </div>
 </div>
 
-<div id="importProgressBox" class="mt-3" style="display:none;">
-    <div class="alert alert-info">
-        <strong>Importing students…</strong>
-        <div class="progress mt-2">
+{{-- =========================
+    FULLSCREEN IMPORT OVERLAY
+========================== --}}
+<div id="importOverlay" style="display:none;">
+    <div class="import-overlay-backdrop"></div>
+
+    <div class="import-overlay-card">
+        <h5>Importing Students</h5>
+        <p class="text-muted">
+            Please wait. Do not refresh or leave this page.
+        </p>
+
+        <div class="progress mb-2">
             <div id="importProgressBar"
                  class="progress-bar progress-bar-striped progress-bar-animated"
                  style="width:0%">0%</div>
         </div>
-        <small id="importProgressText" class="text-muted d-block mt-1">
+
+        <small id="importProgressText" class="text-muted">
             Preparing import…
         </small>
     </div>
 </div>
 
-
+{{-- =========================
+    JAVASCRIPT
+========================== --}}
 <script>
-    const progressUrl = "{{ route('students.import.progress', $my_class->id) }}";
+const progressUrl = "{{ route('students.import.progress', $my_class->id) }}";
+let pollInterval = null;
 
-    $('form[action="{{ route('students.import') }}"]').on('submit', function (e) {
-        e.preventDefault();
+$('form[action="{{ route('students.import') }}"]').on('submit', function (e) {
+    e.preventDefault();
 
-        let formData = new FormData(this);
+    let formData = new FormData(this);
+    lockUI();
+    $('#bulkUploadModal').modal('hide');
 
-        $('#uploadBtn').prop('disabled', true).text('Uploading...');
-        $('#bulkUploadModal').modal('hide');
-        $('#importProgressBox').show();
+    $.ajax({
+        url: "{{ route('students.import') }}",
+        method: "POST",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: startPolling,
+        error: unlockUI
+    });
+});
 
-        $.ajax({
-            url: "{{ route('students.import') }}",
-            method: "POST",
-            data: formData,
-            processData: false,
-            contentType: false,
-            success: function () {
-                pollProgress();
+function lockUI() {
+    $('#importOverlay').fadeIn(150);
+    $('.lock-during-import').prop('disabled', true);
+}
+
+function unlockUI() {
+    clearInterval(pollInterval);
+    $('#importOverlay').fadeOut(150);
+    $('.lock-during-import').prop('disabled', false);
+}
+
+function startPolling() {
+    pollInterval = setInterval(() => {
+        $.get(progressUrl, function (data) {
+            if (!data.total) return;
+
+            let percent = Math.round((data.processed / data.total) * 100);
+
+            $('#importProgressBar')
+                .css('width', percent + '%')
+                .text(percent + '%');
+
+            $('#importProgressText')
+                .text(`${data.processed} of ${data.total} students processed`);
+
+            if (data.status === 'done' || data.processed >= data.total) {
+                $('#importProgressBar')
+                    .removeClass('progress-bar-animated')
+                    .addClass('bg-success')
+                    .text('Completed');
+
+                setTimeout(() => {
+                    unlockUI();
+                    location.reload();
+                }, 1200);
             }
         });
+    }, 1200);
+}
+
+// Resume progress if page refreshed mid-import
+$(document).ready(function () {
+    $.get(progressUrl, function (data) {
+        if (data.status === 'running' || data.status === 'queued') {
+            lockUI();
+            startPolling();
+        }
     });
-
-    function pollProgress() {
-        const interval = setInterval(() => {
-            $.get(progressUrl, function (data) {
-
-                if (!data.total) return;
-
-                let percent = Math.round((data.processed / data.total) * 100);
-
-                $('#importProgressBar')
-                    .css('width', percent + '%')
-                    .text(percent + '%');
-
-                $('#importProgressText')
-                    .text(`${data.processed} of ${data.total} processed`);
-
-                if (data.status === 'done') {
-                    clearInterval(interval);
-
-                    $('#importProgressBar')
-                        .removeClass('progress-bar-animated')
-                        .addClass('bg-success')
-                        .text('Completed');
-
-                    $('#importProgressText').text('Import completed successfully.');
-
-                    setTimeout(() => location.reload(), 1500);
-                }
-            });
-        }, 1500);
-    }
+});
 </script>
 
-
-
-
-
 @endsection
+
+
+
+
